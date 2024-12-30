78868681bbaaa7029d441fff80d1faea0e74c288
melanmeg <naoya05280708@gmail.com>
2024-12-31 01:15:19 +0900

    fix

--- terraform/enable_api.tf
+++ terraform/enable_api.tf
@@ -1,20 +1,12 @@
 resource "google_project_service" "enabled_apis" {
   for_each = toset([
     "cloudkms.googleapis.com",
-    "secretmanager.googleapis.com"
+    "cloudresourcemanager.googleapis.com",
+    "iam.googleapis.com",
+    "iamcredentials.googleapis.com",
+    "secretmanager.googleapis.com",
+    "storage.googleapis.com",
   ])
   project = "my-project-melanmeg"
-  service = each.value
+  service = each.key
 }
-
-# cloudresourcemanager.googleapis.com
-# secretmanager.googleapis.com
-# cloudkms.googleapis.com
-# iam.googleapis.com
-
-# resource "google_project_iam_member" "service_account_admin" {
-#   project = "my-project-melanmeg"
-
-#   role   = "roles/iam.serviceAccountAdmin"
-#   member = "serviceAccount:terraform@my-project-melanmeg.iam.gserviceaccount.com"
-# }
