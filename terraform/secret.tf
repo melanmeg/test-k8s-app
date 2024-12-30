503af25678a620e2bf3d197ce74c409597241965
melanmeg <naoya05280708@gmail.com>
2024-12-30 23:51:38 +0900

    fix

--- terraform/secret.tf
+++ terraform/secret.tf
@@ -1,11 +1,18 @@
 resource "google_secret_manager_secret" "argocd_github_token" {
   secret_id = "argocd-github-token-sm"
   replication {
-    automatic = true
+    user_managed {
+      replicas {
+        location = "asia-northeast1"
+      }
+      replicas {
+        location = "asia-southeast1"
+      }
+    }
   }
 }
 
 resource "google_secret_manager_secret_version" "argocd_github_token" {
   secret = google_secret_manager_secret.argocd_github_token.id
-  secret_data = file("db-password.secret")
+  secret_data = file("argocd_github_token.secret")
 }
