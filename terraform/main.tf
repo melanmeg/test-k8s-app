f7b7e17775cbf7343c92c027a028fb682559df7f
melanmeg <naoya05280708@gmail.com>
2024-12-31 01:46:24 +0900

    fix

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,16 +2,5 @@ resource "google_storage_bucket" "my_bucket" {
   name          = "my-unique-bucket-name-hogehoge"
   location      = "US"
   force_destroy = true
-
   uniform_bucket_level_access = true
-
-  lifecycle_rule {
-    action {
-      type = "Delete"
-    }
-
-    condition {
-      age = 365
-    }
-  }
 }
