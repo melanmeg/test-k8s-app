6e96f29a3770a249239624177a9f9721545ef8da
melanmeg <naoya05280708@gmail.com>
2024-12-31 01:47:17 +0900

    fix

--- terraform/main.tf
+++
@@ -1,6 +0,0 @@
-resource "google_storage_bucket" "my_bucket" {
-  name          = "my-unique-bucket-name-hogehoge"
-  location      = "US"
-  force_destroy = true
-  uniform_bucket_level_access = true
-}
