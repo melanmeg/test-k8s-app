5243865eba7dc908e2b59023dcb446ff34c55e02
melanmeg <naoya05280708@gmail.com>
2024-12-31 00:56:02 +0900

    fix

--- terraform/enable_api.tf
+++ terraform/enable_api.tf
@@ -6,3 +6,15 @@ resource "google_project_service" "enabled_apis" {
   project = "my-project-melanmeg"
   service = each.value
 }
+
+# cloudresourcemanager.googleapis.com
+# secretmanager.googleapis.com
+# cloudkms.googleapis.com
+# iam.googleapis.com
+
+# resource "google_project_iam_member" "service_account_admin" {
+#   project = "my-project-melanmeg"
+
+#   role   = "roles/iam.serviceAccountAdmin"
+#   member = "serviceAccount:terraform@my-project-melanmeg.iam.gserviceaccount.com"
+# }
