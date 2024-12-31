db51e1961b87f55a4775e8194def59cd186d32ef
melanmeg <naoya05280708@gmail.com>
2025-01-01 02:22:47 +0900

    fix

--- terraform/gcs.tf
+++ terraform/gcs.tf
@@ -21,13 +21,3 @@ resource "google_storage_bucket" "tfstate_bucket" {
     }
   }
 }
-
-resource "google_storage_bucket" "sample_bucket" {
-  project       = local.project_id
-  name          = "${local.project_id}-tfstate-hogehoge"
-  location      = local.region
-  storage_class = "REGIONAL"
-  force_destroy = false
-
-  uniform_bucket_level_access = true
-}
