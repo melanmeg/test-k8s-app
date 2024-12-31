0bc40953d58d8f41d60fc1ff8f851095e4eec6e3
melanmeg <naoya05280708@gmail.com>
2025-01-01 01:15:57 +0900

    fix

--- terraform/wip.tf
+++ terraform/wip.tf
@@ -32,12 +32,41 @@ resource "google_service_account_iam_member" "workload_identity_member" {
 }
 
 resource "google_project_iam_member" "owner" {
-  project = "my-project-melanmeg"
+  project = local.project_id
   role    = "roles/owner"
   member  = "serviceAccount:${google_service_account.github.email}"
 }
 
-
 ############################################
 # External Secrets Pool Provider           #
 ############################################
+resource "google_iam_workload_identity_pool_provider" "external_secrets_pool_provider" {
+  project                            = local.project_id
+  workload_identity_pool_id          = google_iam_workload_identity_pool.my_pool.workload_identity_pool_id
+  workload_identity_pool_provider_id = "external-secrets-pool-provider"
+  attribute_mapping = {
+    "google.subject"             = "assertion.sub"
+    "attribute.repository"       = "assertion.repository"
+    "attribute.repository_owner" = "assertion.repository_owner"
+  }
+  # attribute_condition = "assertion.repository == '${local.repository}'"
+  # oidc {
+  #   issuer_uri = "https://token.actions.githubusercontent.com"
+  # }
+}
+
+resource "google_service_account" "external-secrets" {
+  account_id   = "external-secrets"
+}
+
+resource "google_service_account_iam_member" "workload_identity_member" {
+  service_account_id = google_service_account.external-secrets.name
+  role               = "roles/iam.workloadIdentityUser"
+  member             = "principalSet://iam.googleapis.com/${google_iam_workload_identity_pool.my_pool.name}/attribute.repository/${local.repository}"
+}
+
+resource "google_project_iam_member" "owner" {
+  project = local.project_id
+  role    = "roles/owner"
+  member  = "serviceAccount:${google_service_account.external-secrets.email}"
+}
