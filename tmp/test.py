b974517bea30464343202975e88734bb2dbbd9a3
melanmeg <naoya05280708@gmail.com>
2025-01-13 04:29:34 +0900

    fix

--- tmp/test.py
+++ tmp/test.py
@@ -2,12 +2,24 @@ import pika
 
 credentials = pika.PlainCredentials("admin", "admin")
 connection = pika.BlockingConnection(
-    pika.ConnectionParameters(host="192.168.11.161", port=8091, credentials=credentials)
+    pika.ConnectionParameters(host="192.168.11.161",
+                              port=8091, credentials=credentials)
 )
 channel = connection.channel()
 
-channel.queue_declare(queue="my_queue")
+channel.queue_declare(queue="my_queue2")
 print("チャンネルが作成されました")
+
+
+def callback(ch, method, properties, body):
+  print(f"受信したメッセージ: {body}")
+
+
+channel.basic_consume(
+    queue="my_queue2", on_message_callback=callback, auto_ack=True)
+
+print("メッセージを待機しています...")
+channel.start_consuming()
 
 channel.close()
 connection.close()
