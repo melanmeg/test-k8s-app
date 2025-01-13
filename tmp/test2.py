fdf6b655956d9d2a5463c547a592a27d562418ca
melanmeg <naoya05280708@gmail.com>
2025-01-13 16:45:04 +0900

    fix

--- tmp/test2.py
+++ tmp/test2.py
@@ -3,11 +3,7 @@ import pika
 # RabbitMQ接続設定
 credentials = pika.PlainCredentials("admin", "admin")
 connection = pika.BlockingConnection(
-    pika.ConnectionParameters(
-        host="192.168.11.161",
-        port=8091,
-        credentials=credentials
-    )
+    pika.ConnectionParameters(host="192.168.11.161", port=8091, credentials=credentials)
 )
 channel = connection.channel()
 
@@ -17,9 +13,9 @@ channel.queue_declare(queue="my_queue")
 # メッセージ送信
 message = "テストメッセージ"
 channel.basic_publish(
-    exchange='',               # デフォルトのエクスチェンジを使用
-    routing_key='my_queue',   # 宛先キューの名前
-    body=message               # メッセージの内容
+    exchange="",  # デフォルトのエクスチェンジを使用
+    routing_key="my_queue",  # 宛先キューの名前
+    body=message,  # メッセージの内容
 )
 print(f"メッセージを送信しました: {message}")
 
